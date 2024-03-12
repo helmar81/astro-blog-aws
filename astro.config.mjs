b57e80a47e89c194a0702e1e3e62d66a8a740573
helmar81 <helmar81@gmail.com>
2024-03-12 18:28:23 +0100

    update site commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'http://astro-aws2024.s3-website.eu-central-1.amazonaws.com/',
   integrations: [mdx(), sitemap(), tailwind()]
+  
 });
+
